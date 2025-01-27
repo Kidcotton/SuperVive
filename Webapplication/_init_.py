980ca2f551fc6fbb8cec74eba0d97a757fc20a47
Vaporized <zacharykwok74@gmail.com>
2025-01-27 16:56:54 +0800

    e

--- Webapplication/_init_.py
+++ Webapplication/_init_.py
@@ -127,9 +127,6 @@ def populate():
     return redirect(url_for('index'))
 
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-
 @app.route('/')
 def home():
     return render_template("home.html")
